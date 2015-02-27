80060d181d9d00a29d355c89e0acfa5204b1e56b
Wynand <groendrakie@gmail.com>
2015-02-27 15:36:33 +0100

    First clean build

---
+++ expert-box-language.js
@@ -0,0 +1,130 @@
+'use strict';
+
+var expertBoxLanguage = angular.module('expert-box-language', ['ngResource']);
+
+//language service
+expertBoxLanguage.service('ebLanguage', ['$log', '$q', '$resource',
+                                         function($log, $q, $resource) {
+  var self = this;
+
+  //function to load a set of translations for language code
+  self.load = function(code) {
+    $log.debug('language.load ' + code);
+
+    //create language file resource
+    var languageFilePath = 'translation/' + code.toLowerCase() + '.json';
+    $log.debug('languageFilePath is '+languageFilePath);
+    var resource = $resource(languageFilePath,null, {
+      'query': {
+          method:'GET',
+          isArray:false,
+          cache:true
+      }
+    });
+
+    //translations is a promise
+    var defer = $q.defer();
+    self.translations = resource.query(function(data) {
+      self.translations = data;
+      $log.debug('ebLanguage.load.query.callback : self.translations = ...');
+      $log.debug(self.translations);
+    }).$promise;
+
+    //return promise to resolve after translations is recieved and stored
+    $log.debug('ebLanguage.load : self.translations = ...');
+    $log.debug(self.translations);
+    return self.translations;
+  };
+
+  //form a noun as singular or plural according to a number
+  //return promise until translations are resolved
+  self.formNoun = function (number, singular, plural) {
+      $log.debug('ebLanguage.formNoun');
+
+      var defer = $q.defer();
+      var promise = defer.promise;
+      
+      $q.when(self.translations).then(function (translations) {        
+        $log.debug('ebLanguage.formNoun : when translations then');
+
+        if (angular.isUndefined(number) || angular.isUndefined(singular) || angular.isUndefined(plural)) {
+            var msg = 'ebLanguage.formNoun input validation failed'
+            $log.error(msg);
+            defer.reject(msg);
+        }
+        else if (number<2) {
+          $log.debug('ebLanguage.formNoun resolves as '+translations[singular]);
+          defer.resolve(translations[singular]);
+        }
+        else {
+          $log.debug('ebLanguage.formNoun resolves as '+translations[plural]);
+          defer.resolve(translations[plural]);
+        }
+      });
+      $log.debug(promise);
+      return promise;
+  };
+
+  //translates a measurement with formed noun (singular/plural) units
+  //use: measurements that contain a number and a formed noun 
+  //  example: 1 gram, 500 grams
+  //  example: 1 person, 2 people
+  //  example: 1 hour, 2 hours
+  //  example: 1 beer, 2 beers
+  //arguments:
+  //  singular: translation key for singulat form of the noun/unit
+  //  plural: translation key for plural form of the noun/unit
+  //return promise until translations are resolved    
+  self.measurement = function (number, singular, plural) {
+      var deferred = $q.defer();
+      $q.when(self.translations).then(function () {
+          if (angular.isUndefined(number) || angular.isUndefined(singular) || angular.isUndefined(plural)) {
+              deferred.reject('measurement input validation failed');
+          }
+          $q.when(self.formNoun(number, singular, plural)).then(function(noun) {
+              deferred.resolve(number+' '+noun);
+          });            
+      });
+      return deferred.promise;
+  };
+
+  //creates a comma seperated list of translated items, with last item joined by conjective add translation
+  //argument items should be an array of strings
+  self.list = function (items) {
+      var deferred = $q.defer();
+      $q.when(translations).then(function () {
+          var strings = _.map(items, function (item) {
+              var translation = translations[item];
+              return angular.isDefined(translation) ? translation : item;
+          });
+
+          var firstString = _.first(strings);
+
+          var midString = '';
+          var lastString = '';
+          if (items.length > 1) {
+              var midStrings = strings.slice(1, -1);
+              midString = _.reduce(midStrings, function (memo, str) {
+                  return memo + ', ' + str;
+              }, '');
+              lastString = ' ' + translations.AND + ' ' + _.last(strings);
+          }
+
+          deferred.resolve(firstString + midString + lastString);
+      });
+      return deferred.promise;
+  };
+
+  self.getTranslation = function(key) {
+      var deferred = $q.defer();
+      $q.when(translations).then(function () {
+          deferred.resolve(translations[key]);
+      });
+      return deferred.promise;
+  };
+
+  //init language service
+  $log.debug('init language service');
+  var deferred = $q.defer();
+  self.translations = deferred.promise;
+}]);
---
+++ test/unit/expert-box-language-spec.js
@@ -0,0 +1,177 @@
+'use strict';
+
+describe('expert-box-language', function() {
+
+	describe('ebLanguage', function() {		
+
+		beforeEach(module('ngResource'));
+
+		//load expert box language module
+		beforeEach(module('expert-box-language', function ($provide) {
+			//$provide.value('$log', console);
+		}));
+
+		var $httpBackend, $log, $rootScope, $q, ebLanguage;
+		beforeEach(inject(function ($injector, _$log_,_$rootScope_, _$q_, _ebLanguage_) {
+			$httpBackend = $injector.get('$httpBackend');
+			$log = _$log_;
+			$rootScope = _$rootScope_;
+			$q = _$q_;
+			ebLanguage = _ebLanguage_;
+		}));
+
+		//content for 2 different translation files
+		var translationsEN = {
+			ONE: 'one',
+			TWO: 'two',
+			BEER: 'beer',
+			BEERS: 'beers',
+			AND: 'and'
+		};
+		var translationsNL = {
+			ONE: 'een',
+			TWO: 'twee',
+			BEER: 'biertje',
+			BEERS: 'biertjes',
+			AND: 'en'
+		};
+
+		it('should resolve translation promise', function (){
+
+			//ebLanguage should get file during loading
+			$httpBackend.expectGET('translation/en.json').respond(translationsEN);
+			
+			//load the language
+			ebLanguage.load('EN');
+			$httpBackend.flush();
+			
+			expect(ebLanguage.translations.ONE).toEqual(translationsEN.ONE);
+		});
+
+		it('should form singular noun', function (done){
+
+			//ebLanguage should get file during loading
+			$httpBackend.expectGET('translation/nl.json').respond(translationsNL);
+			
+			//load the language
+			ebLanguage.load('NL');
+			$httpBackend.flush();
+
+			var tests = {
+				success : function (noun) {
+					$log.debug('then success');
+					expect(noun).toEqual('biertje');
+				},
+				failure : function (error) {
+					$log.debug('catch failure');
+					expect(error).toBeUndefined();
+				}
+			};
+
+			spyOn(tests, 'success').andCallThrough();
+			spyOn(tests, 'failure').andCallThrough();
+
+			var formNoun = ebLanguage.formNoun(1,'BEER','BEERS');
+			$q.when(formNoun).then(tests.success).catch(tests.failure).finally(done);
+			$rootScope.$apply();
+			$rootScope.$apply();
+
+			expect(tests.success).toHaveBeenCalled();
+		});
+
+		it('should form plural noun', function (done){
+
+			//ebLanguage should get file during loading
+			$httpBackend.expectGET('translation/en.json').respond(translationsEN);
+			
+			//load the language
+			ebLanguage.load('EN');
+			$httpBackend.flush();
+
+			var tests = {
+				success : function (noun) {
+					$log.debug('then success');
+					expect(noun).toEqual('beers');
+				},
+				failure : function (error) {
+					$log.debug('catch failure');
+					expect(error).toBeUndefined();
+				}
+			};
+
+			spyOn(tests, 'success').andCallThrough();
+			spyOn(tests, 'failure').andCallThrough();
+
+			var formNoun = ebLanguage.formNoun(2,'BEER','BEERS');
+			$q.when(formNoun).then(tests.success).catch(tests.failure).finally(done);
+			$rootScope.$apply();
+			$rootScope.$apply();
+
+			expect(tests.success).toHaveBeenCalled();
+		});
+
+		it('should create measurement with unit as a singular formed noun', function (done){
+
+			//ebLanguage should get file during loading
+			$httpBackend.expectGET('translation/nl.json').respond(translationsNL);
+			
+			//load the language
+			ebLanguage.load('NL');
+			$httpBackend.flush();
+
+			var tests = {
+				success : function (measurement) {
+					$log.debug('then success '+measurement);
+					expect(measurement).toEqual('1 biertje');
+				},
+				failure : function (error) {
+					$log.debug('catch failure');
+					expect(error).toBeUndefined();
+				}
+			};
+
+			spyOn(tests, 'success').andCallThrough();
+			spyOn(tests, 'failure').andCallThrough();
+
+			var measurement = ebLanguage.measurement(1,'BEER','BEERS');
+			$q.when(measurement).then(tests.success).catch(tests.failure).finally(done);
+			$rootScope.$apply();
+			$rootScope.$apply();
+
+			expect(tests.success).toHaveBeenCalled();
+		});
+
+		it('should create measurement with unit as a plural formed noun', function (done){
+
+			//ebLanguage should get file during loading
+			$httpBackend.expectGET('translation/en.json').respond(translationsEN);
+			
+			//load the language
+			ebLanguage.load('EN');
+			$httpBackend.flush();
+
+			var tests = {
+				success : function (measurement) {
+					$log.debug('then success '+measurement);
+					expect(measurement).toEqual('2 beers');
+				},
+				failure : function (error) {
+					$log.debug('catch failure');
+					expect(error).toBeUndefined();
+				}
+			};
+
+			spyOn(tests, 'success').andCallThrough();
+			spyOn(tests, 'failure').andCallThrough();
+
+			var measurement = ebLanguage.measurement(2,'BEER','BEERS');
+			$q.when(measurement).then(tests.success).catch(tests.failure).finally(done);
+			$rootScope.$apply();
+			$rootScope.$apply();
+
+			expect(tests.success).toHaveBeenCalled();
+		});
+	});
+});
+
+
